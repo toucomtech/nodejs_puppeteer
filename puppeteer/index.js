c44396f459351107347417afd8fa62d4c2b2703e
David Tougaw <42074677+DavidTou@users.noreply.github.com>
2020-04-17 11:31:54 -0500

    fix(url): fixed test url

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -10,7 +10,7 @@ const puppeteer = require('puppeteer-core');
 (async () => {
   const browser = await puppeteer.launch({defaultViewport: {height: 1080, width: 1920}, headless: true, args: ['--display=:1', '--no-sandbox', '--disable-extensions'], executablePath: '/usr/bin/chromium-browser'});
   const page = await browser.newPage()
-  await page.goto('https://www.toucomtech.com/')
+  await page.goto('https://toucomtech.com/')
   const title = await page.title()
   console.log(title)
   await browser.close()
