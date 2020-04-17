3550bad0acc3bc93ec2e7626b96f036c702f54f3
David Tougaw <dmtougaw@gmail.com>
2020-04-17 20:18:34 +0100

    feat(error catch): try-catch added to puppet script

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -8,10 +8,18 @@
 const puppeteer = require('puppeteer-core');
 
 (async () => {
-  const browser = await puppeteer.launch({defaultViewport: {height: 1080, width: 1920}, headless: true, args: ['--display=:1', '--no-sandbox', '--disable-extensions'], executablePath: '/usr/bin/chromium-browser'});
-  const page = await browser.newPage()
-  await page.goto('https://toucomtech.com/')
-  const title = await page.title()
-  console.log(title)
-  await browser.close()
+  	const browser = await puppeteer.launch({defaultViewport: {height: 1080, width: 1920}, headless: true, args: ['--display=:1', '--no-sandbox', '--disable-extensions'], executablePath: '/usr/bin/chromium-browser'});
+  	const page = await browser.newPage();
+  	try{
+
+		await page.goto('https://toucomtech.com/');
+  		const title = await page.title();
+  		console.log(title);
+  		await browser.close();
+		console.log('OK');
+	}
+	catch(err) {
+		console.log(err.message);
+		console.log('ERROR');
+	}
 })()
